582996ba1fbf58b9f9873e93a9fd6ce3d071166e
Matias Ignacio Guzman <101336792+ElMabre@users.noreply.github.com>
2026-05-27 19:42:48 -0400

    Clean up OrderRequest.java by removing conflict markers
    Removed unnecessary merge conflict markers from OrderRequest.java.

--- order-service/src/main/java/com/smartlogix/order/dto/OrderRequest.java
+++ order-service/src/main/java/com/smartlogix/order/dto/OrderRequest.java
@@ -47,8 +47,4 @@ public class OrderRequest {
         @Positive(message = "La cantidad debe ser mayor a cero")
         private Integer quantity;
     }
-<<<<<<< HEAD
-}
-=======
-}
->>>>>>> 800946a (Se modificaron los comentarios en codigo y readme.md)
+
