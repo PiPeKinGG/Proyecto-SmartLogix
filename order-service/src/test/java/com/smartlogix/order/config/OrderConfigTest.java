c612bbeb9ffb5f49e0a983ef79af69bd95596bf0
Matias Ignacio Guzman <101336792+ElMabre@users.noreply.github.com>
2026-05-27 20:00:47 -0400

    Rename test and update to SecurityConfig

--- order-service/src/test/java/com/smartlogix/order/config/OrderConfigTest.java
+++ order-service/src/test/java/com/smartlogix/order/config/OrderConfigTest.java
@@ -1,12 +1,15 @@
 package com.smartlogix.order.config;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import static org.junit.jupiter.api.Assertions.*;
 
 class OrderConfigTest {
     @Test
-    void testOrderConfigLoads() {
-        OrderConfig config = new OrderConfig();
-        assertNotNull(config);
+    void testSecurityConfigLoads() {
+        SecurityConfig config = new SecurityConfig();
+        BCryptPasswordEncoder encoder = config.passwordEncoder();
+        
+        assertNotNull(encoder);
     }
 }
